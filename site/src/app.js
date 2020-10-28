3539d4fd6e8c0bd14060c8d0d4a1863085b7de5c
Eduardo <edubaccarezza@gmail.com>
2020-10-28 18:07:09 -0300

    Se agrega estructura basica app con node y express

---
+++ site/src/app.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+app.use(express.static('./public'))
+
+app.get ('/', function (req,res) {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+})
+
+app.get ('/producto', function (req,res) {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+})
+
+app.get ('/carrito', function (req,res) {
+    res.sendFile(path.join(__dirname, './views/productCart.html'));
+})
+
+app.get ('/login', function (req,res) {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+
+app.get ('/register', function (req,res) {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+})
+
+app.listen (3000, function() {
+    console.log ("El servidor esta corriendo en el puerto 3000");
+})
