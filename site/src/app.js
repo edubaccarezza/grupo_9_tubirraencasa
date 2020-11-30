8f1390e41878475a3ad6ed43074836af5a33dd74
Eduardo <edubaccarezza@gmail.com>
2020-11-29 21:06:09 -0300

    Se actualizo el Header, footer y pagina de inicio

--- site/src/app.js
+++ site/src/app.js
@@ -1,22 +1,32 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
+const methodOverride = require ('method-override')
 
 const mainRouter = require('./routes/main');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 
-//Cuchame, Express... Vas a usar de motor de vista EJS...
+//EJS
 app.set('view engine', 'ejs');
-//Cuchame, Express.. Hay una carpeta que tiene todas las vistas. Es esta...
 app.set('views', path.join(__dirname, 'views'));
 
+//OVERRIDE
+app.use(methodOverride('_method'));
+
+//PUBLIC
 app.use(express.static(path.join(__dirname, '../public')))
 
+//
+app.use(express.urlencoded({extended : false}))
+app.use(express.json())
+
+//RUTAS
 app.use('/', mainRouter)
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
+//PUERTOS
 app.listen(process.env.PORT || 3000, function() {
     console.log("El servidor está corriendo en el puerto 3000");
     console.log("-------------------");
