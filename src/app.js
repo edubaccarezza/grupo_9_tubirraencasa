5661d485d2341e6f96aea9d1160051e8f4a260fa
JuaniNoto <juani.noto26@gmail.com>
2021-01-11 11:20:25 -0300

    Checkeando la creacion de cookie

--- src/app.js
+++ src/app.js
@@ -26,10 +26,10 @@ app.use(express.static(path.join(__dirname, '../public')))
 //
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
-app.use(rememberMiddleware);
 app.use(cookieParser());
 app.use(session( { secret: 'laBirra' } ));
 app.use(sesionIniciadaMiddleware);
+app.use(rememberMiddleware);
 
 //RUTAS
 app.use('/', mainRouter)
--- src/middlewares/rememberMiddleware.js
+++ src/middlewares/rememberMiddleware.js
@@ -1,9 +1,29 @@
+const fs = require('fs');
+const path = require ('path');
+
+let users = fs.readFileSync(path.join(__dirname, '../database/users.json'), 'utf8');
+users = JSON.parse(users);
+
 function rememberMiddleware (req, res, next) {
     next();
 
     if (req.cookies.remember != undefined && req.session.user == undefined) {
-        req.session.user = req.cookies.remember;
+        //req.session.user = req.cookies.remember;
+        let userLoginIn;
+
+        for(let i=0; i<users.length; i++) {
+            if (users.email == req.cookies.remember){    
+                
+                userLoginIn = users;
+                    
+            }
+        }
+
+        req.session.user = userLoginIn;
+
     }
+
 }
+
 
 module.exports = rememberMiddleware;
