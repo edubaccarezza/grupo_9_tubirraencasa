9925a6d449b4907c172094166e2a74fcf27e65cb
JuaniNoto <juani.noto26@gmail.com>
2021-01-28 17:33:03 -0300

    Users controller modificado

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,127 +1,157 @@
-const fs = require('fs');
-const path = require ('path');
 const bcrypt = require ('bcrypt');
 const {validationResult} = require('express-validator')
+const db = require ('../database/models')
 
-// let users = fs.readFileSync(path.join(__dirname, '../database/users.json'), 'utf8');
-// users = JSON.parse(users);
+ module.exports = {
 
-// let ultimoId = 0;
-// for (let i = 0; i < users.length; i++) {
-//     if (ultimoId < users[i].id) {
-//         ultimoId = users[i].id
-//     }
-// }
+     register: function(req, res) {
+         res.render('users/register')
+     }, 
 
-// module.exports = {
-//     register: function(req, res) {
-//         res.render('users/register')
-//     }, 
-//     create: function(req,res){
-//         let errors = validationResult(req)
-//         if(errors.isEmpty()){
-//             if(req.body.password==req.body.repassword){
-//                     let nuevoUsuario = {
-//                         id:ultimoId + 1,
-//                         firstName:req.body.firstName,
-//                         lastName:req.body.lastName,
-//                         email:req.body.email,
-//                         password:bcrypt.hashSync(req.body.password,12),
-//                         image:req.file.filename
-//                     }
-
-//                     users.push(nuevoUsuario);
-//                     fs.writeFileSync(path.join(__dirname, '../database/users.json'),JSON.stringify(users, null, 4));
-            
-//                     req.session.user = nuevoUsuario;
-//                     return res.redirect('/');
+     create: function(req,res){
+         let errors = validationResult(req)
+         if(errors.isEmpty()){
+             if(req.body.password==req.body.repassword){
+                db.Usuarios.create({
+                    nombre:req.body.firstName,
+                    apellido:req.body.lastName,
+                    email:req.body.email,
+                    password:bcrypt.hashSync(req.body.password,12),
+                    imagen:req.file.filename,
+                    admin: 1
+                })
+                .then(function(datos){
+                    console.log(datos); //-------------A MIRAR
+                    req.session.user = datos; // A CORREGIR ----------------------------------------
+                })
+                .catch(function(error){
+                    res.send(error);
+                })  
+                return res.redirect('/');         
                 
-//             }else{
-//                  return res.render('users/register',
-//                     {
-//                         old:req.body,
-//                         passError:[
-//                         {msg:'Las contraseñas no coinciden'}]
-//                     })
-//             }
+                
+             }else{
+                  return res.render('users/register',
+                     {
+                         old:req.body,
+                         passError:[
+                         {msg:'Las contraseñas no coinciden'}]
+                     })
+             }
     
             
-//         } else{
-//             if(req.body.password==req.body.repassword){
-//                 return res.render('users/register',{
-//                     errors:errors.mapped(),
-//                     old:req.body
-//                 })
-//             }else{
-//                 return res.render('users/register',{
-//                     errors:errors.mapped(),
-//                     passError:[
-//                         {msg:'Las contraseñas no coinciden'}],
-//                     old:req.body
-//                 })
-//             }
-//         }
-//     },
-//     login: function(req, res) {
-//         res.render('users/login');
-//     },
-//     loginIn: function(req,res){
+         } else{
 
-//         let errors = validationResult(req);
-//         let { email, password, remember } = req.body;
-//         if (errors.isEmpty()) {
-//             let userLoginIn;
+             if(req.body.password==req.body.repassword){
+                 return res.render('users/register',{
+                     errors:errors.mapped(),
+                     old:req.body
+                 })
+             } else{
+                 return res.render('users/register',{
+                     errors:errors.mapped(),
+                     passError:[
+                         {msg:'Las contraseñas no coinciden'}],
+                     old:req.body
+                 })
+             }
+         }
+     },
 
-//             users.forEach(user => {
-//                 if (user.email == email){    
-//                     if(bcrypt.compareSync(password, user.password)) {
-//                         userLoginIn = user;
-                        
-//                     }
-//                 }
-//             });
+     login: function(req, res) {
+         res.render('users/login');
+     },
 
-//             if (userLoginIn == undefined) {
-//                 return res.render('users/login',
-//             {noCoincidence:[
-//                 {msg:'El email o la contraseña son incorrectas'}]});
+     loginIn: function(req,res){
+
+         let errors = validationResult(req);
+         let { email, password, remember } = req.body;
+         if (errors.isEmpty()) {
+             let userLoginIn;
+
+             users.forEach(user => {
+                 if (user.email == email){    
+                     if(bcrypt.compareSync(password, user.password)) {
+                         userLoginIn = user;  
+                     }
+                 }
+             });
+
+             if (userLoginIn == undefined) {
+                 return res.render('users/login',
+                 {noCoincidence:[
+                 {msg:'El email o la contraseña son incorrectas'}]});
                 
 
-//             } 
+            } 
 
-//             req.session.user = userLoginIn;
+             req.session.user = userLoginIn;
 
-//             if (remember != undefined) {
-//                 res.cookie('remember', userLoginIn.email, { maxAge: 60000 });
-//             }
-//             return res.redirect('/');
+             if (remember != undefined) {
+                 res.cookie('remember', userLoginIn.email, { maxAge: 60000 });
+             }
+             return res.redirect('/');
 
-//         } else {           
-//             return res.render('users/login',
-//                 {
-//                     errors:errors.mapped(),
-//                     old:req.body 
-//                 });
-//             }
-//     },
+         } else {           
+             return res.render('users/login',
+                 {
+                     errors:errors.mapped(),
+                     old:req.body 
+                 });
+             }
+     },
 
-//     logOut:function(req,res){
-//         req.session.destroy();
-//         res.redirect('/');
-//     },
+     logOut:function(req,res){
+         req.session.destroy();
+         res.redirect('/');
+     },
 
-//     edit:function(req,res){
-//         return res.render('users/edit.ejs');
-//     },
+     edit:function(req,res){
+         db.Usuarios.findByPk(req.params.id)
+         .then(function(usuario){
+            return res.render('users/edit.ejs',{usuario:usuario});
+         })
+         
+     },
 
-//     post: function(req,res){
+     update: function(req,res){
+        let errors = validationResult(req)
+        if(errors.isEmpty()){
+                db.Usuarios.update({
+                firstName:req.body.firstName,
+                lastName:req.body.lastName,
+                email:req.body.email,
+                password:bcrypt.hashSync(req.body.password,12),
+                admin:1
+            },{
+                where:{
+                    id:req.params.id
+                }
+            })
+            .then(function(datos){
+                console.log(datos); //-------------A MIRAR
+                req.session.user = datos; // A CORREGIR ----------------------------------------
+            })
+            .catch(function(error){
+                res.send(error);
+            })  
+            return res.redirect('/');   
+
+        } else { return res.render('users/register',{
+            errors:errors.mapped(),
+            old:req.body
+            })
+        }
         
-//     },
+     },
 
-//     delete: function(req,res){
-
-//         //FALTA CODIGO QUE BORRE USUARIO
-//         req.session.destroy();
-//         res.redirect('/');
-//     }
-// }
+     delete: function(req,res){
+         db.Usuarios.destroy({
+             where:{
+                 id:req.params.id
+             }
+         });
+         req.session.destroy();
+         res.redirect('/');
+     }
+ }
