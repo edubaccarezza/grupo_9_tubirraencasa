6c6440fcaa054ddc1064f4e1629ad61f64529c35
JuaniNoto <juani.noto26@gmail.com>
2021-02-09 22:15:47 -0300

    correccion login validation

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,32 +1,70 @@
 window.addEventListener("load",function(){
+
+    console.log('HOLAAAA');
+
+    //Formulario
     let formulario = document.querySelector("#formulario"); 
-    formulario.addEventListener("submit",function(event){ 
-        event.preventDefault();
-        let errors = {};
-        let email = document.querySelector("#email");
-        let errorMail = document.querySelector('#errorEmail')
-        let regex= new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-        if(email.value.length  == 0){
-            errors.email = "El campo email debe estar completo"
-            errorMail.innerText = errors.email
-        
-        }else if(!regex.test(email.value)){
-            errors.email = "Por favor ingrese un mail valido"
-            errorMail.innerText =  errors.email
-        }
-        let password = document.querySelector("#password");
-        let errorPassword = document.querySelector('#errorPassword')
+    //Inputs
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
 
-        if(password.value.length == 0){
-            
-            errors.password = "El campo de contraseña debe estar completo"
-            errorPassword.innerText = errors.password 
-        }
-        if(Object.keys(errors).length > 0 ){
-        
-        }else{
-            formulario.submit();
+    //Small
+    let smallEmail = document.querySelector('#errorEmail');
+    let smallPassword = document.querySelector('#errorPassword');
+
+    //Errors
+    let errors = {
+        email:true,
+        password:true
+    };
+
+    //regEx
+    let regExEmail =  /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+    let regExPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+
+    //Validacion email
+    email.addEventListener('blur',function(){
+        if(!regExEmail.test(email.value)){
+            errors.email = true;
+            smallEmail.innerText = 'Debe ingresar un email valido';
+        }else {
+            smallEmail.innerText = '';
+            errors.email = false;
         }
     })
+
+    //Validacion Password
+    password.addEventListener('blur',function(){
+        if(!regExPassword.test(password.value)){
+            errors.password = true;
+            smallPassword.innerText = 'La contraseña debe tener como mínimo 1 mayúscula, 1 minúscula, 1 numero, 1 caracter especial' ;
+        }else {
+            smallPassword.innerText = '';
+            errors.password = false;
+        }
+    })
+
+    formulario.addEventListener('submit',function(event){
+       
+        console.log(errors);
+
+        event.preventDefault();
+        
+        let flag = false;
+        
+        for(let i in errors){
+           if(errors[i] == true){
+               flag = true;
+               break;
+           }
+        }
+
+        if(flag == false){
+            form.submit();
+        }
+
+    });
+
+
 })
