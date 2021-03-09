65ced58a5d1018c498e0a2c3a449b617bccb7272
edubaccarezza <edubaccarezza@gmail.com>
2021-03-09 19:39:56 -0300

    Nuevas actualizaciones

---
+++ public/js/carrito/carrito.js
@@ -0,0 +1,13 @@
+class Carrito {
+
+    // Anadir productos al carrito
+
+    comprarProducto (e) {
+        e.preventDefault();
+        if(e.target.classList.contains('buttons')) {
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatos.Producto(producto    )
+        }
+    }
+
+}
